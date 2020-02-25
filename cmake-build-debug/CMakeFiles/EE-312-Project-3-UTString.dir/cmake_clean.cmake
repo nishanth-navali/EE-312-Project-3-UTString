869e4cf5043dbb722fdc0c6dabf3839e7e7eeb5d
nishanth-navali <nishanth.navali@utexas.edu>
2020-02-25 14:59:05 -0600

    fix comp

---
+++ cmake-build-debug/CMakeFiles/EE-312-Project-3-UTString.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EE-312-Project-3-UTString.dir/Project3.cpp.o"
+  "CMakeFiles/EE-312-Project-3-UTString.dir/main.cpp.o"
+  "EE-312-Project-3-UTString"
+  "EE-312-Project-3-UTString.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EE-312-Project-3-UTString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
